15ed5f0b73ed58cf255cd7622213b1a17b441482
Ibrahim Özhan <89545837+IbrahimOezhan@users.noreply.github.com>
2025-06-20 19:07:31 +0200

    Fixed

--- Editor/Scripts/DropdownDrawer.cs
+++ Editor/Scripts/DropdownDrawer.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 
@@ -16,21 +17,25 @@ namespace IbrahKit
             }
 
             DropdownAttribute dropdownAttribute = (DropdownAttribute)attribute;
+            string filePath = dropdownAttribute.filePath;
 
-            List<string> dropdownOptions = new();
-
-            if (dropdownAttribute.error.Item1)
+            if (!File.Exists(filePath))
             {
-                EditorGUI.LabelField(position, label.text,dropdownAttribute.error.Item2);
+                EditorGUI.LabelField(position, label.text, "File doesn't exist");
                 return;
-
             }
 
-            dropdownOptions.AddRange(dropdownAttribute.dropdownInput);
+            List<string> dropdownInput = String_Utilities.GetDropdown(filePath);
 
-            int selectedIndex = Mathf.Max(0, dropdownOptions.IndexOf(property.stringValue));
-            selectedIndex = EditorGUI.Popup(position, label.text, selectedIndex, dropdownOptions.ToArray());
-            property.stringValue = dropdownOptions[selectedIndex];
+            if (dropdownInput == null || dropdownInput.Count == 0)
+            {
+                EditorGUI.LabelField(position, label.text, "No options");
+                return;
+            }
+
+            int selectedIndex = Mathf.Max(0, dropdownInput.IndexOf(property.stringValue));
+            selectedIndex = EditorGUI.Popup(position, label.text, selectedIndex, dropdownInput.ToArray());
+            property.stringValue = dropdownInput[selectedIndex];
         }
     }
 }
--- Runtime/Scripts/Dropdown/DropdownAttribute.cs
+++ Runtime/Scripts/Dropdown/DropdownAttribute.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
 using UnityEngine;
 
 namespace IbrahKit
@@ -8,29 +6,11 @@ namespace IbrahKit
     [AttributeUsage(AttributeTargets.Field, Inherited = true, AllowMultiple = false)]
     public class DropdownAttribute : PropertyAttribute
     {
-        public List<string> dropdownInput = new();
-        public (bool, string) error;
+        public string filePath;
 
-        public DropdownAttribute(string filePath)
+        public DropdownAttribute(string fileName)
         {
-            filePath = "Assets/Resources/DropdownFiles/" + filePath + ".txt";
-
-            if (!File.Exists(filePath))
-            {
-                error = (true, "File doesnt exist");
-            }
-
-            dropdownInput = String_Utilities.GetDropdown(filePath);
-
-            if (dropdownInput.Count == 0)
-            {
-                error = (true, "No options");
-            }
-        }
-
-        public DropdownAttribute(List<string> input)
-        {
-            dropdownInput = new(input);
+            this.filePath = "Assets/Resources/DropdownFiles/" + fileName + ".txt";
         }
     }
 }
