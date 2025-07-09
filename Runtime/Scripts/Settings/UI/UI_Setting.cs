658e887fc37ce1c0cc71c88c8505605131657eb3
Ibrahim Özhan <89545837+IbrahimOezhan@users.noreply.github.com>
2025-07-09 10:09:44 +0200

    Update UI_Setting.cs

--- Runtime/Scripts/Settings/UI/UI_Setting.cs
+++ Runtime/Scripts/Settings/UI/UI_Setting.cs
@@ -84,7 +84,7 @@ namespace IbrahKit
 
         public virtual void ChangeValue(float _value)
         {
-            setting.SetValue(_value);
+            setting.SetValue(setting.GetValue() + _value);
             setting.ApplyChanges();
             UpdateUI();
         }
